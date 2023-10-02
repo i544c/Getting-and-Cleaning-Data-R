17de75ca673ea0ac7cc96d7e61eb75ea2eb6a5e2
Isaac <104391905+i544c@users.noreply.github.com>
2023-10-02 11:58:44 -0300

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,65 +1,68 @@
-# Autor: Isaac Gomes Veras
-# Data de criação: 29 de setembro de 2023
+# Johns Hopkins University - Getting and Cleaning Data - Coursera
+# Activity: Obtaining and cleaning data - Course project
+# Author: Isaac Gomes Veras
+# Creation date: September 29, 2023
 # IDE: DataSpell 2023.2.2 Build #DS-232.9921.48, built on September 13, 2023
-# Johns Hopikins University - Getting and Cleaning Data
-# Atividade: Obtenção e limpeza de dados Projeto do curso
+# R Version: 4.3.1
 
-R.version.string; cat("\n") # Versão do R
-getwd(); cat("\n") # Diretório de trabalho
+R.version.string; cat("\n") # Check R version
+getwd(); cat("\n")          # Check working directory
 
-# Instalação de pacotes:
-
+# Package installation:
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load (pacman,		# gerenciador de pacotes;
-                tidyverse,	# conjunto de ferramentas para Data Science;
+pacman::p_load (pacman,		# pacman = package manager
+                tidyverse	# tidyverse = set of tools for Data Science
 )
 
-#---- Banco de dados: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+#---- Data Base: Human Activity Recognition Using Smartphones Dataset
+#---- Link: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
 
-#---- Etapa 1: Mesclar os conjuntos de treinamento e teste para criar um conjunto de dados.
-print("Etapa 1: Mesclar os conjuntos de treinamento e teste para criar um conjunto de dados:")
+#---- Step 1: Merge the training and testing sets to create a dataset:
 
-  # Extrair dados "train":
+  # # Extract "train" data:
   s_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names= "subject")
   y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names= "activity")
   x_train <- read.table("UCI HAR Dataset/train/X_train.txt", comment.char="")
   d_train <- cbind(s_train, y_train, x_train)
-    print(d_train)
+  
+  head(d_train) # data verification
 
-  # Extrair dados "test":
+  # # Extract "test" data:
   s_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names= "subject")
   y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names= "activity")
   x_test <- read.table("UCI HAR Dataset/test/X_test.txt", comment.char="")
   d_test <- cbind(s_test, y_test, x_test)
-    print(d_test)
+  
+  head(d_test) # data verification
 
-  # Mesclando os dados:
+  # Merging the data:
   df <- rbind(d_train, d_test)
-    print(df)
+  
+  head(df) # data verification
 
-  # Recursos de leitura:
+  # Reading Features:
   feature_names <- read.table("UCI HAR Dataset/features.txt", col.names=c("id", "name"))
   features <- c("subject", "activity", as.vector(feature_names[,"name"]))
-    print(features)
+  
+  head(features) # data verification
 
-#---- Etapa 2: Extrair somente as medidas sobre a media e o desvio padrao de cada medida:
-print("Etapa 2: Extrair somente as medidas sobre a media e o desvio padrao de cada medida:")
+#---- Step 2: Extract only the measurements about the mean and standard deviation of each measurement:
 
 filtered_feature_index <- grepl("mean|std|subject|activity", features) & !grepl("meanFreq", features)
 filtered_data <- df[, filtered_feature_index]
-  print(filtered_data)
 
-#---- Etapa 3: Usar nomes descritivos de atividades para nomear as atividades no conjunto de dados:
-print("Etapa 3: Usar nomes descritivos de atividades para nomear as atividades no conjunto de dados:")
+head(filtered_data)
+
+#---- Step 3: Use descriptive activity names to name the activities in the dataset:
 
 activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names=c("id", "name"))
 for (i in seq_len(nrow(activities))) {
   filtered_data$activity[filtered_data$activity == activities[i, "id"]] <- as.character(activities[i, "name"])
 }
-  print(filtered_data$activity)
 
-#---- Etapa 4: Rotular adequadamente o conjunto de dados com nomes descritivos de variaveis:
-print("Etapa 4: Rotular adequadamente o conjunto de dados com nomes descritivos de variaveis:")
+head(filtered_data$activity) # data verification
+
+#---- Step 4: Appropriately label the dataset with descriptive variable names:
 
 filtered_names <- features[filtered_feature_index]
 filtered_names <- gsub("\\(\\)", "", filtered_names)
@@ -71,17 +74,16 @@ filtered_names <- gsub("^t(.*)$", "\\1-time", filtered_names)
 filtered_names <- gsub("^f(.*)$", "\\1-frequency", filtered_names)
 filtered_names <- tolower(filtered_names)
 
-  # Atribuir nomes filtrados aos recursos:
+  # Assign filtered names to resources:
   names(filtered_data) <- filtered_names
-  print(filtered_names)
+  head(filtered_names) # data verification
 
-#---- Etapa 5: A partir do conjunto de dados da etapa 4, crie um segundo conjunto de dados independente e organizado com a media de cada variavel para cada atividade e cada sujeito:
-print("A partir do conjunto de dados da etapa 4, crie um segundo conjunto de dados independente e organizado com a media de cada variavel para cada atividade e cada sujeito:")
+#---- Step 5: From the data set from step 4, create a second independent and organized data set with the average of each variable for each activity and each subject:
 
 tidy_dataset <- tbl_df(filtered_data) %>%
   group_by(subject, activity) %>%
   summarise_each(funs(mean))
   print(tidy_dataset)
 
-# Salvar "new_data" na pasta:
+# Save "tidy_dataset" in folder:
   write.table(tidy_dataset, file="tidy_dataset.txt", row.name=FALSE)
